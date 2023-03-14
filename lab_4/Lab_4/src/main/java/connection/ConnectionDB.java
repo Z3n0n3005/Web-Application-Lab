5feda97749dd415bce32d1516f101dca00024650
Vy <z3n0n3005@gmail.com>
2023-03-14 15:34:59 +0700

    Small fix

--- lab_4/Lab_4/src/main/java/connection/ConnectionDB.java
+++ lab_4/Lab_4/src/main/java/connection/ConnectionDB.java
@@ -15,6 +15,6 @@ import java.sql.SQLException;
 public class ConnectionDB {
     public static Connection getCon() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/lab4", "root", "Eudaimonia+3119");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/lab4", "root", "password");
     }
 }
